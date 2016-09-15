8b3654b6b4c35477b9c78777ea8a58a5edb0deb9
Scott Harvey <scottyharvey33@gmil.com>
2016-09-15 12:16:57 -0600

    Added Nav

---
+++ js/directives/navBar.js
@@ -0,0 +1,16 @@
+angular.module("tesla").directive("navBar", function(){
+  return {
+    templateUrl: '',
+    restric: "EA",
+    scope: {
+
+    },
+    controller: function($scope){
+
+    },
+    link: function(scope, element, attrs){
+
+    }
+
+  }
+})
